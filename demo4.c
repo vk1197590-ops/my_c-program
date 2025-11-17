763e49302f007ce788ff6d072cd4d651ff5d3a56
vk1197590-ops <vk1197590@gmail.com>
2025-11-17 19:05:16 +0530

    Create demo4.c

---
+++ demo4.c
@@ -0,0 +1,9 @@
+print uesing your name, age, location 
+#include <stdio.h>
+
+int main() {
+    printf("Name: Varsha K A\n");
+    printf("Age: 20\n");
+    printf("Location: Karnataka\n");
+    return 0;
+}
