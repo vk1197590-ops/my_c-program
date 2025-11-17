49f50c02dd7643c44aaaac793dee84cfe2ea428b
vk1197590-ops <vk1197590@gmail.com>
2025-11-17 16:27:49 +0530

    Create demo1.c

---
+++ demo1.c
@@ -0,0 +1,12 @@
+sum of a and b
+#include<stdio.h>
+int main()
+{
+    int a,b,sum;
+    printf("entre the value of a ");
+    scanf("%d",&a);
+    printf("enter the value of b");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("sum of %d",sum);
+}
