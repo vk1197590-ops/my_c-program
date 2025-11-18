6ad792829fd2e332bf2e9963987f4a2106d8da96
vk1197590-ops <vk1197590@gmail.com>
2025-11-18 22:24:47 +0530

    Create c demo18.c

---
+++ c demo18.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main ()
+{
+    int a,b,c;
+    printf ("enter the of the three number \n");
+if (" a<=b && a<=c")
+{
+    printf("%d is the smallest number\n",a);
+}
+else if ("b<=a && b<=c")
+{
+    printf ("%d is the smallest number \n",b);
+}
+else 
+{
+    printf ("%d is the smallest number\n",c);
+}
+}
