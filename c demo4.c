2c0fe3c57f65e6b920e930a36d7d17b7a5a231b7
vk1197590-ops <vk1197590@gmail.com>
2025-11-18 15:03:08 +0530

    Create c demo4.c

---
+++ c demo4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int userdetails()
+{
+ printf("MY NAME IS : VARSHA\n");
+ printf("MY AGE IS : 19\n");
+ printf("MY DEPARMENT IS: CSE \n");
+}
+int main()
+{
+    userdetails();
+}
