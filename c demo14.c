97ce71f3ca830fb2c508039cc9a7e583feb2d711
vk1197590-ops <vk1197590@gmail.com>
2025-11-18 21:06:11 +0530

    Create c demo14.c

---
+++ c demo14.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    float radius, area;
+    float pi=3.14;
+    printf ("enter the radius of the circle");
+    scanf("%f",& radius);
+    
+    area= pi* radius* radius;
+    printf("aera of the circle=%f",area);
+}
