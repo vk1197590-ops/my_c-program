4cf663722156485f4bb4443b9aacb6d97f1cf095
vk1197590-ops <vk1197590@gmail.com>
2025-11-18 19:35:20 +0530

    Create c demo8.c

---
+++ c demo8.c
@@ -0,0 +1,14 @@
+include<stdio.h>
+int main()
+{
+    int n;
+    printf ("enter value of the n\n");
+    scanf ("%d\n",&n);
+    if(n%2==0){
+        printf("the number is a even number");
+    }
+        else 
+        {printf("the number is a odd number");
+        }
+    }
+
