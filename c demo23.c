7282feb6d85c032dd8e4fb1ae46942c216833e8a
vk1197590-ops <vk1197590@gmail.com>
2025-11-18 22:53:11 +0530

    Create c demo23.c

---
+++ c demo23.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num >= 10 && num <= 100) {
+        printf("%d is within the range 10 to 100.\n", num);
+    } else {
+        printf("%d is outside the range 10 to 100.\n", num);
+    }
+
+    return 0;
+}
