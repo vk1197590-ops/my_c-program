baf6463e5b878c0089983c42a3be0ce72afffc74
vk1197590-ops <vk1197590@gmail.com>
2025-11-18 19:20:47 +0530

    Update c demo7.c

--- c demo7.c
+++ c demo7.c
@@ -1,13 +1,16 @@
-#include<stdio.h>
-int main()
-{
-    int a,b;
-    printf("enter value of a and b\n");
-    scanf("%d\n",&a,&b);
-    int sum, difference,product;
-    float quotient;
-    sum=a+b;
-    difference=a-b;
-    product=a*b;
-    quotient=a/b;
-   printf("sum=%d\n,difference=%d\n,product=%d\n,quotient=%f\n",sum,difference,product,quotient");
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("Sum = %d\n", a + b);
+    printf("Difference = %d\n", a - b);
+    printf("Product = %d\n", a * b);
+
+    printf("Quotient = %d\n", a / b);  // assumes b is not zero
+
+    return 0;
+}
