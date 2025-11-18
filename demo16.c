9945bb9d7eb426c3728f51955f19387a53cf8ec3
vk1197590-ops <vk1197590@gmail.com>
2025-11-18 12:20:03 +0530

    Create demo16.c

---
+++ demo16.c
@@ -0,0 +1,28 @@
+ if ladder
+# include<stdio.h>
+int main()
+{
+    int marks=90;
+    {
+    if (marks>=90)
+        printf("distinction");
+    }
+    
+     if(else (marks>=75)
+     {
+        printf("good");
+    }
+    if(else (marks>=55)
+   {
+        printf("first class");
+    }
+   if(else (marks>=35)
+  {
+        printf("senond class");
+    }
+
+else
+{
+    printf("fila");
+}
+}
