68454e13aaf40680df25b490df0b69956ea803ab
vk1197590-ops <vk1197590@gmail.com>
2025-11-18 21:54:36 +0530

    Create c demo16.c

---
+++ c demo16.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int num=15;
+    printf("enter value of number");
+if ("15 % 3==0 && 15 % 5==0")
+{
+    printf ("%d is divisible by both 3 and 5",15);
+}
+else 
+{
+    printf ("%d is not divisible by both 3 and 5",15);
+}
+}
