78003228b224e22e59adab56e939225a3df2b8a4
vk1197590-ops <vk1197590@gmail.com>
2025-11-18 12:36:45 +0530

    Create demo18.c

---
+++ demo18.c
@@ -0,0 +1,12 @@
+tables
+# include<stdio.h>
+int main()
+{
+   int num;
+   printf("enter the value of num");
+   scanf("%d",&num);
+   for (int i=1;i<=10;i++)
+   {
+       printf("%d*%d=%d\n",num,i,num*i);
+   }
+}
