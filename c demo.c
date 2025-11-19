f476fa8aba3ec7b2b62aca2d1d3975cb4cc3020a
vk1197590-ops <vk1197590@gmail.com>
2025-11-19 14:12:53 +0530

    Create c demo.c

---
+++ c demo.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    printf("Enter number of rows: ");
+    scanf("%d", &r);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &c);
+
+    int a[r][c], b[r][c], sum[r][c];
+    printf("\nEnter elements of first matrix:\n");
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    printf("\nEnter elements of second matrix:\n");
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
+            sum[i][j] = a[i][j] + b[i][j];
+        }
+    }
+
+    // Display result
+    printf("\nResultant Matrix (Addition):\n");
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
