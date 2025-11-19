562c93e2e5f3a996162f4f1f627ca8017119ba2e
vk1197590-ops <vk1197590@gmail.com>
2025-11-19 11:27:13 +0530

    Create a demo3.c

---
+++ a demo3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+int rose [2][3]={ {1,2,3} ,
+                  {4,5,6}
+};
+for (int i=0;i<2;i++)
+{
+    for(int j=0;j<3;j++)
+    {
+      printf ("%d\n",rose[i][j]);  
+    }
+}
+}
