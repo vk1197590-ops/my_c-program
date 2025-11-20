61569af5e2cde909293ab8665f8c438c99e96b82
vk1197590-ops <vk1197590@gmail.com>
2025-11-20 10:28:24 +0530

    Add demo1.c for basic C input/output example

---
+++ p demo1.c
@@ -0,0 +1,10 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    char pen[20];
+    printf ("enter the array=");
+    scanf ("%s",pen);
+    printf ("%s",pen);
+}
