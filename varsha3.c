b4cd5c284612d039f39c6a5928ad93763544cb98
vk1197590-ops <vk1197590@gmail.com>
2025-11-21 15:42:51 +0530

    Create varsha3.c

---
+++ varsha3.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int id ;
+    int PAN Number, AADHAR_Number, APAAR_Id, Driving License, Passport;
+    printf("enter the id like PAN Number, AADHAR_Number, APAAR_Id, Driving License, Passport\n");
+    scanf("%d" , &PAN Number, &AADHAR_Number, &APAAR_Id, &Driving License, &Passport);
+    if(id=123456)
+    {
+        printf("the individual is verified\n");
+    }
+    else if(id=678910)
+    {
+        printf("the individual is verifired\n");
+    }
+    else if(id=123456)
+    {
+        printf("the individual is verifired\n");
+    }
+    else if(id=78910)
+    {
+        printf("the individual is verifired\n");
+    }
+    else if(id=123456)
+    {
+        printf("the individual is verifired\n");
+    }
+    else 
+    {
+        printf("the individual is not verifired\n");
+    }
+}
